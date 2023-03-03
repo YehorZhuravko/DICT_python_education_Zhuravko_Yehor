17854878ebfa6c7ad3f5fc6f99541ce101c485f6
YehorZhuravko <y.a.zhuravko@student.khai.edu>
2023-03-03 22:09:38 +0200

    Tic-tac-toe stage 7

--- Tictactoe/Tictactoe.py
+++ Tictactoe/Tictactoe.py
@@ -1,75 +1,63 @@
-print("Welcome to tictactoe game :)")
-board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-
-winner = None
-full = False
-moves = []
-game = True
+board = [' ' for _ in range(9)]
 
 
-def display():
-    for i in board:
-        print("-" * 7)
-        for j in i:
-            print("|" + j, end="")
-        print("|")
-    print("-" * 7)
-
-display()
+# Функція друку ігрового поля
+def print_board():
+    print(f"| {board[0]} | {board[1]} | {board[2]} |")
+    print(f"| {board[3]} | {board[4]} | {board[5]} |")
+    print(f"| {board[6]} | {board[7]} | {board[8]} |")
 
 
-def win(player):
-    global winner
-    b = board
-    hori = b[0][0] == b[0][1] == b[0][2] != ' ' or b[1][0] == b[1][1] == b[1][2] != ' ' or b[2][0] == b[2][1] == b[2][
-        2] != ' '
-    verti = b[0][0] == b[1][0] == b[2][0] != ' ' or b[0][1] == b[1][1] == b[2][1] != ' ' or b[0][2] == b[1][2] == b[2][
-        2] != ' '
-    dia = b[0][0] == b[1][1] == b[2][2] != ' ' or b[2][0] == b[1][1] == b[0][2] != ' '
-    if hori or verti or dia:
-        winner = player
-    for i in board:
-        if " " in i:
-            full = False
+# Функція перевірки виграшу
+def check_win(mark):
+    # Перевірка рядків
+    for i in range(0, 9, 3):
+        if board[i] == board[i + 1] == board[i + 2] == mark:
+            return True
+    # Перевірка стовпців
+    for i in range(3):
+        if board[i] == board[i + 3] == board[i + 6] == mark:
+            return True
+    # Перевірка діагоналей
+    if board[0] == board[4] == board[8] == mark:
+        return True
+    if board[2] == board[4] == board[6] == mark:
+        return True
+    return False
+
+
+# Старт гри
+def play_game():
+    print("Ласкаво просимо до гри Хрестики-ноліки!")
+    print_board()
+    while True:
+
+        # Хід гравця 1
+        choice = int(input("Гравець 1, виберіть квадрат (1-9): ")) - 1
+        if board[choice] == ' ':
+            board[choice] = 'X'
+        else:
+            print("Вибачте, цей квадрат уже зайнятий.")
+            continue
+        print_board()
+        if check_win('X'):
+            print("Вітаю! Гравець 1 виграє!")
             break
+        if ' ' not in board:
+            print("Це нічия!")
+            break
+
+        # Хід гравця 2
+        choice = int(input("Гравець 2, виберіть квадрат (1-9): ")) - 1
+        if board[choice] == ' ':
+            board[choice] = 'O'
         else:
-            full = True
-    if full and not winner:
-        winner = 'draw'
+            print("Вибачте, цей квадрат уже зайнятий.")
+            continue
+        print_board()
+        if check_win('O'):
+            print("Вітаю! Гравець 2 виграє!")
+            break
 
 
-player = 'X'
-while game:
-    move = input(f"Player ({player}) coordinates: ")
-    if move not in moves:
-        moves.append(move)
-    else:
-        print("Already Occupied!")
-        continue
-    try:
-        y, x = int(move[0]), int(move[1])
-
-        x -= 1
-        if y == 1:
-            y = 2
-        elif y == 3:
-            y = 0
-        else:
-            y -= 1
-    except:
-        print("Enter the Coordinates properly!")
-        continue
-    board[2 - y][x] = f"{player}"
-
-    display()
-    win(player)
-    if winner == player:
-        print(f"{player} wins!!!")
-        game = False
-    elif winner == "draw":
-        print("It's a Tie")
-        game = False
-    if player == 'X':
-        player = 'O'
-    else:
-        player = 'X'
+play_game()
