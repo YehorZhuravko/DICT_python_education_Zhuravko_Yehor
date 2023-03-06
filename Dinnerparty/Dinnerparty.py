3ab50aa0e43309c7c892179e190e338195f4bbb6
YehorZhuravko <y.a.zhuravko@student.khai.edu>
2023-03-06 15:28:54 +0200

    Виправлений Dinnerparty

--- Dinnerparty/Dinnerparty.py
+++ Dinnerparty/Dinnerparty.py
@@ -1,74 +1,37 @@
-import string
 import random
 
-friend_q = None
-friends_list = {}
-total_amount = None
-divided = None
-lucky_one = None
+# Кількість друзів
+num_friends = int(input("Enter the number of friends joining (including you): "))
+while int(num_friends) <= 1:
+        print("No one is joined for the party!")
+        exit()
 
-def lucky_one_check():
-    global lucky_one
-    global friend_q
-    choice = input("Who is lucky? feature? Write Yes/No: ")
-    if choice == "Yes":
-        lucky_one = random.randint(0, int(friend_q-1))
-        k = 0
-        for i in friends_list:
-            friends_list[i] = total_amount / (friend_q - 1)
-            if k == lucky_one:
-                friends_list[i] = 0
-                print(i + " is lucky one! :) ")
+# Формування списку
+friends = []
+for i in range(num_friends):
+    name = input("Enter friend #{}'s name: ".format(i+1))
+    friends.append(name)
 
-            k += 1
-    return friends_list
+# Загальна сума грошей і роздрук списку друзів
+total_money = float(input("Enter the total amount of money that your friends brought: "))
+money_per_friend = total_money / num_friends
+print("Your friends are:")
+for friend in friends:
+    print("- " + friend)
 
+# Функція "Who is lucky?"
+answer = input("Do you want to use the 'Who is lucky?' feature? (Yes/No): ")
+if answer.lower() == "no":
+    print("Each friend brought {:.2f} dollars.".format(money_per_friend))
+else:
+    lucky_friend = random.choice(friends)
+    print("{} is the lucky one!".format(lucky_friend))
 
-def check_number(a):
-    while int(a) <= 0:
-        a = input("No one is joined for the party!")
-        raise SystemExit(1)
-
-
-def friend_input():
-    global friend_q
-    friend_q = int(input("Enter the number of friends joining (including you): "))
-    if int(friend_q) <= 0:
-        check_number(friend_q)
-    i = 0
-    while i < int(friend_q):
-        name = input("Enter the name of a person: ")
-        friends_list[name] = 0
-        i += 1
-    return friend_q
-
-
-def total_amount_request():
-    global total_amount
-    total_amount = int(input("Enter the total amount: "))
-    return total_amount
-
-
-def dividing():
-    global divided
-    global total_amount
-    global friend_q
-    divided = total_amount / friend_q
-    for i in friends_list:
-        friends_list[i] = divided
-
-
-def rounding():
-    global friends_list
-    for z in friends_list:
-        friends_list[z] = round(friends_list.get(z), 2)
-    return friends_list
-
-
-friend_input()
-total_amount_request()
-dividing()
-lucky_one_check()
-rounding()
-
-print(friends_list)
+    # Обнулення грошей щасливого друга та поділ їх порівну між рештою друзів
+    m_without_lucky = total_money / (num_friends - 1)
+    print()
+    for friend in friends:
+        if friend != lucky_friend:
+            print("{} gets {:.2f} dollars.".format(friend, m_without_lucky))
+        else:
+            print("{} gets nothing.".format(lucky_friend))
